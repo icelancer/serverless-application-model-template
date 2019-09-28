7d482532eb1e1fe56ae01102de911cd929f7e195
icelancer <SendToGHKim@gmail.com>
2019-09-28 17:21:15 +0900

    fix: 람다 함수명 추가 및 S3 버킷 생성 CloudFormation 작성

--- 00.basic-template/src/index.js
+++ 00.basic-template/src/index.js
@@ -1,5 +1,8 @@
 module.exports.handler = async () => ({
     statusCode: 200,
+    headers: {
+        'Content-Type': 'application/json'
+    },
     body: JSON.stringify( {
         message: "hello serverless application model"
     })
