b4fb429bdda23e8fc72bd2fea903ab03af6a833d
icelancer <SendToGHKim@gmail.com>
2019-09-29 19:54:14 +0900

    feature: API Gateway 설정

---
+++ 01.serverless-api/src/index.js
@@ -0,0 +1,7 @@
+module.exports.handler = async () => ({
+    items: [
+        { title: 'Serverless Application Model', status: 'inprogress'},
+        { title: 'Lmabda Layer', status: 'todo'}
+    ],
+    totalCount: 2
+});
